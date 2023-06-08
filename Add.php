ac1c9c4f57c7404c81ce1d017b3c45e604dc6d8f
srinivasreddy44 <srinivasreddyb044@gmail.com>
2023-06-08 07:53:01 +0530

    Creating addition file

--- Add.php
+++ Add.php
@@ -5,7 +5,7 @@ int main() {
     
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
-
+    // calculate the sum
     // calculate the sum
     sum = number1 + number2;      
     
